2e2b255635c04db1b025af1e0a5869891e89fbd5
AndySantisteban <andyjosue160720@gmail.com>
2023-06-10 18:34:50 -0500

    Feat: Init

---
+++ index.js
@@ -0,0 +1,41 @@
+// server.js
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const activeSockets = new Set();
+
+io.on('connection', (socket) => {
+  activeSockets.add(socket);
+  console.log("socket",socket)
+  socket.on('offer', (offer) => {
+    socket.broadcast.emit('offer', offer);
+    console.log("offer",offer)
+  });
+
+  socket.on('answer', (answer) => {
+    socket.broadcast.emit('answer', answer);
+    console.log("answer",answer)
+  });
+
+  socket.on('iceCandidate', (candidate) => {
+    socket.broadcast.emit('iceCandidate', candidate);
+    console.log("iceCandidate",iceCandidate)
+  });
+
+  socket.on('disconnect', () => {
+    activeSockets.delete(socket);
+    console.log("delete - socket",socket)
+  });
+});
+
+const port = 3001;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
